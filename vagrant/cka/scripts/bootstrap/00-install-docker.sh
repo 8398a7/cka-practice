97f2ff290f6e0a04dac8913e8a433b036fef02ff
839 <8398a7@gmail.com>
2019-06-11 22:54:32 +0900

    setup vagrantfile

---
+++ vagrant/cka/scripts/bootstrap/00-install-docker.sh
@@ -0,0 +1,26 @@
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+apt-get update && apt-get install -y docker-ce=18.06.2~ce~3-0~ubuntu
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
