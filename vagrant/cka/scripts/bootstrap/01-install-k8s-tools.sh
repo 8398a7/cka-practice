b307eb7c32fd61cfffdaa0928f30a3566037aa82
839 <8398a7@gmail.com>
2019-06-13 21:01:14 +0900

    specific k8s version

--- vagrant/cka/scripts/bootstrap/01-install-k8s-tools.sh
+++ vagrant/cka/scripts/bootstrap/01-install-k8s-tools.sh
@@ -1,8 +1,10 @@
+VERSION='1.13.7-00'
+
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION
 apt-mark hold kubelet kubeadm kubectl
