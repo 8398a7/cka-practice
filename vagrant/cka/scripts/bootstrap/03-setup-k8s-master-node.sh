38804992d7bdd46bd0974a74e7fb77810186db5d
839 <8398a7@gmail.com>
2019-06-16 20:28:29 +0900

    remove unused lines

--- vagrant/cka/scripts/bootstrap/03-setup-k8s-master-node.sh
+++ vagrant/cka/scripts/bootstrap/03-setup-k8s-master-node.sh
@@ -2,12 +2,3 @@
 
 # pod-network-cidr for calico
 kubeadm init --apiserver-advertise-address 10.240.0.10 --pod-network-cidr 192.168.0.0/16
-
-# setup kubeconfig
-sudo su - vagrant
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# setup calico
-kubectl apply -f https://docs.projectcalico.org/v3.7/manifests/calico.yaml
